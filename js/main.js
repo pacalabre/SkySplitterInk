eab15a61c8bc42e931eed1fa0ee9b4b288201d76
Paul <pacalabre@gmail.com>
2015-07-26 16:55:21 -0700

    updated the footer to be consistent across pages, added scroll gallery before trying instagram feed gallery

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$( function(){
+    $( "#gallery" ).jGallery( {
+        "transition":"rotateCubeLeftOut_rotateCubeLeftIn",
+        "transitionBackward":"rotateCubeRightOut_rotateCubeRightIn",
+        "transitionCols":"1",
+        "transitionRows":"1",
+        "thumbnailsPosition":"right",
+        "thumbType":"image",
+        "backgroundColor":"FFFFFF",
+        "textColor":"000000",
+        "mode":"standard"
+    } );
+} );
