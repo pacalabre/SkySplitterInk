eb508b8ddc07372da9716c5265c0caa830302ce6
Paul <pacalabre@gmail.com>
2015-08-02 17:01:33 -0700

    added and positioned gallery on photo page

--- js/responsiveNav.js
+++ js/responsiveNav.js
@@ -9,3 +9,19 @@ jQuery(document).ready(function() {
         e.preventDefault();
     });
 });
+
+/* Gallery */
+
+ $( function(){
+    $( "#gallery" ).jGallery( {
+        "transition":"moveToLeftFade_moveFromRightFade",
+        "transitionBackward":"moveToRightFade_moveFromLeftFade",
+        "transitionCols":"1",
+        "transitionRows":"1",
+        "thumbnailsPosition":"bottom",
+        "thumbType":"image",
+        "backgroundColor":"FFFFFF",
+        "textColor":"000000",
+        "mode":"standard"
+    } );
+} );
